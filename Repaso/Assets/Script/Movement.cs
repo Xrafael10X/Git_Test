a9d12b906998dd7f6c66924fe9839618748aba53
Xrafael10X <xrafael101104x@gmail.com>
2023-09-27 10:19:19 +0200

    Update movement

--- Repaso/Assets/Script/Movement.cs
+++ Repaso/Assets/Script/Movement.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Vector3 position;
+    public Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called 100 times per second
+    private void FixedUpdate()
     {
         
     }
